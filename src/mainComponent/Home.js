a33c0ef393fee29d59f8cf07efe77aeaf2fb81ab
Muhammad Arbaz <28670muhammadarbaz@gmail.com>
2024-12-13 14:55:24 +0500

    002

--- src/mainComponent/Home.js
+++ src/mainComponent/Home.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+<div className='outerDiv'>
+<div className='innerDiv'>
+<h1>
+  Hello React
+</h1>
+</div>
+</div>
+    </>
   )
 }
