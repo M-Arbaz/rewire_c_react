5c44be4cfd48d885cc174a33dbaafd2b9b6f495e
Muhammad Arbaz <28670muhammadarbaz@gmail.com>
2024-12-16 11:54:49 +0500

    issueresolved

--- src/subComponent/FaqAccordian.js
+++ src/subComponent/FaqAccordian.js
@@ -12,7 +12,7 @@ export default function FaqAccordian() {
     }
   }
 
-  const svg = <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-6n4r9j" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIosIcon"><path d="M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"></path></svg>
+  const svg = <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-6n4r9j" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIosIcon"><path d="M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"></path></svg>
 
   return (
     <div className='accordian'>
